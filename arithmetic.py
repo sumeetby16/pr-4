5a246475dbd5b60dc95790f6b37e2e9c4aa91f6c
sumeetby16 <sumeetby16>
2025-10-22 23:00:34 +0530

    added div,mul

--- arithmetic.py
+++ arithmetic.py
@@ -2,3 +2,6 @@ a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 print("Addition:", a+b)
 print("Subtraction:", a-b)
+
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
